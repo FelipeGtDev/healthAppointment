75d2ba59c37b65b0b570c16e61cc68251e5c5a26
Felipe Lima <audionoulb@gmail.com>
2023-07-10 15:54:13 -0300

    criados testes de patient

---
+++ src/test/java/com/healthAppointment/healthAppointment/service/impl/PatientServiceTest.java
@@ -0,0 +1,265 @@
+package com.healthAppointment.healthAppointment.service.impl;
+
+import com.healthAppointment.healthAppointment.model.Patient;
+import com.healthAppointment.healthAppointment.model.dto.PatientDTO;
+import com.healthAppointment.healthAppointment.repository.PatientRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class PatientServiceTest {
+    @Mock
+    private PatientRepository repository;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    private PatientUtils patientUtils;
+
+    @InjectMocks
+    private PatientService patientService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    public PatientServiceTest() {
+        this.patientUtils = new PatientUtils();
+    }
+
+    @Test
+    void save_shouldReturnPatientDTO() throws ParseException {
+
+
+        // Arrange
+        PatientDTO patientDTO = patientUtils.createPatientDTO1();
+        Patient patient = patientUtils.createPatient1();
+
+        when(modelMapper.map(patientDTO, Patient.class)).thenReturn(patient);
+        when(repository.save(patient)).thenReturn(patient);
+        when(modelMapper.map(patient, PatientDTO.class)).thenReturn(patientDTO);
+
+        // Act
+        PatientDTO response = patientService.save(patientDTO);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(patientDTO, response);
+        assertEquals(patientDTO.getId(), response.getId());
+        assertEquals(patientDTO.getBirthDate(), response.getBirthDate());
+        assertEquals(patientDTO.getCpf(), response.getCpf());
+        assertEquals(patientDTO.getName(), response.getName());
+
+
+        verify(repository, times(1)).save(patient);
+
+
+    }
+
+    @Test
+    void findAllActive_shouldReturnPatientDTOPage() {
+        // Arrange
+        Pageable pageable = mock(Pageable.class);
+        Page<Patient> patients = new PageImpl<>(new ArrayList<>());
+        Page<PatientDTO> expectedResponse = new PageImpl<>(new ArrayList<>());
+
+        when(repository.findAllActive(pageable)).thenReturn(patients);
+        when(modelMapper.map(any(Patient.class), eq(PatientDTO.class))).thenReturn(new PatientDTO());
+
+        // Act
+        Page<PatientDTO> response = patientService.findAllActive(pageable);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(expectedResponse, response);
+        verify(repository, times(1)).findAllActive(pageable);
+    }
+
+    @Test
+    void findAllInactive_shouldReturnPatientDTOPage() {
+        // Arrange
+        Pageable pageable = mock(Pageable.class);
+        Page<Patient> patients = new PageImpl<>(new ArrayList<>());
+        Page<PatientDTO> expectedResponse = new PageImpl<>(new ArrayList<>());
+
+        when(repository.findAllInactive(pageable)).thenReturn(patients);
+        when(modelMapper.map(any(Patient.class), eq(PatientDTO.class))).thenReturn(new PatientDTO());
+
+        // Act
+        Page<PatientDTO> response = patientService.findAllInactive(pageable);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(expectedResponse, response);
+        verify(repository, times(1)).findAllInactive(pageable);
+    }
+
+    @Test
+    void findAll_shouldReturnPatientDTOPage() throws ParseException {
+
+        Patient patient1 = patientUtils.createPatient1();
+        Patient patient2 = patientUtils.createPatient2();
+
+        PatientDTO patientDTO1 = patientUtils.createPatientDTO1();
+        PatientDTO patientDTO2 = patientUtils.createPatientDTO2();
+
+        // Arrange
+        Pageable pageable = mock(Pageable.class);
+        List patientsList = new ArrayList();
+        patientsList.add(patient1);
+        patientsList.add(patient2);
+
+        List patientsDTOList = new ArrayList();
+        patientsDTOList.add(patientDTO1);
+        patientsDTOList.add(patientDTO2);
+
+        Page<Patient> patients = new PageImpl<>(patientsList);
+        Page<PatientDTO> expectedResponse = new PageImpl<>(patientsDTOList);
+
+        when(repository.findAll(pageable)).thenReturn(patients);
+        when(modelMapper.map(patient1, PatientDTO.class)).thenReturn(patientDTO1);
+        when(modelMapper.map(patient2, PatientDTO.class)).thenReturn(patientDTO2);
+
+        // Act
+        Page<PatientDTO> response = patientService.findAll(pageable);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(expectedResponse, response);
+        verify(repository, times(1)).findAll(pageable);
+    }
+
+    @Test
+    void findById_shouldReturnPatientDTO() throws Exception {
+        // Arrange
+        String id = "1";
+        Patient patient = new Patient();
+        PatientDTO expectedResponse = new PatientDTO();
+
+        when(repository.findById(id)).thenReturn(Optional.of(patient));
+        when(modelMapper.map(patient, PatientDTO.class)).thenReturn(expectedResponse);
+
+        // Act
+        PatientDTO response = patientService.findById(id);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(expectedResponse, response);
+        verify(repository, times(1)).findById(id);
+    }
+
+    @Test
+    void findById_shouldThrowExceptionWhenPatientNotFound() {
+        // Arrange
+        String id = "1";
+
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(Exception.class, () -> patientService.findById(id));
+        verify(repository, times(1)).findById(id);
+    }
+
+    @Test
+    void findByName_shouldReturnPatientDTOPage() {
+        // Arrange
+        String name = "John Doe";
+        Pageable pageable = mock(Pageable.class);
+        Page<Patient> patients = new PageImpl<>(new ArrayList<>());
+        Page<PatientDTO> expectedResponse = new PageImpl<>(new ArrayList<>());
+
+        when(repository.findByName(name, pageable)).thenReturn(patients);
+        when(modelMapper.map(any(Patient.class), eq(PatientDTO.class))).thenReturn(new PatientDTO());
+
+        // Act
+        Page<PatientDTO> response = patientService.findByName(name, pageable);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(expectedResponse, response);
+        verify(repository, times(1)).findByName(name, pageable);
+    }
+
+    @Test
+    void update_shouldReturnUpdatedPatientDTO() {
+        // Arrange
+        String id = "1";
+        PatientDTO request = new PatientDTO();
+        Patient existingPatient = new Patient();
+        Patient updatedPatient = new Patient();
+        PatientDTO expectedResponse = new PatientDTO();
+
+        when(repository.findById(id)).thenReturn(Optional.of(existingPatient));
+        when(modelMapper.map(request, Patient.class)).thenReturn(updatedPatient);
+        when(repository.save(updatedPatient)).thenReturn(updatedPatient);
+        when(modelMapper.map(updatedPatient, PatientDTO.class)).thenReturn(expectedResponse);
+
+        // Act
+        PatientDTO response = patientService.update(id, request);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(expectedResponse, response);
+        verify(repository, times(1)).findById(id);
+        verify(repository, times(1)).save(updatedPatient);
+    }
+
+    @Test
+    void update_shouldThrowExceptionWhenPatientNotFound() {
+        // Arrange
+        String id = "1";
+        PatientDTO request = new PatientDTO();
+
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> patientService.update(id, request));
+        verify(repository, times(1)).findById(id);
+    }
+
+    @Test
+    void delete_shouldDeletePatient() {
+        // Arrange
+        String id = "1";
+        Patient patient = new Patient();
+
+        when(repository.findById(id)).thenReturn(Optional.of(patient));
+
+        // Act
+        patientService.delete(id);
+
+        // Assert
+        verify(repository, times(1)).findById(id);
+        verify(repository, times(1)).delete(patient);
+    }
+
+    @Test
+    void delete_shouldThrowExceptionWhenPatientNotFound() {
+        // Arrange
+        String id = "1";
+
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> patientService.delete(id));
+        verify(repository, times(1)).findById(id);
+    }
+
+
+}
---
+++ src/test/java/com/healthAppointment/healthAppointment/service/impl/PatientUtils.java
@@ -0,0 +1,75 @@
+package com.healthAppointment.healthAppointment.service.impl;
+
+import com.healthAppointment.healthAppointment.model.*;
+import com.healthAppointment.healthAppointment.model.dto.*;
+
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PatientUtils {
+
+    public Patient createPatient1() throws ParseException {
+        Patient patient = new Patient();
+        patient.setId("1");
+        patient.setName(new HumanName(null, "John Doe", null));
+        patient.setAddress(new Address("Rua 1", "123", "apt 201", "Bairro 1", "Cidade 1", "Estado 1", "12345678"));
+        patient.setContacts(new Contacts("qwe@email.com", new ArrayList<>(Arrays.asList(
+                new Phone("21", "12345678901", true),
+                new Phone("11", "12345678901", false)))));
+        patient.setBirthDate("01/01/2000");
+        patient.setActive(true);
+        patient.setCreatedAt(LocalDateTime.now());
+        patient.setCpf("12345678901");
+
+        return patient;
+    }
+
+    public Patient createPatient2() throws ParseException {
+        Patient patient = new Patient();
+
+        patient.setId("2");
+        patient.setName(new HumanName(null, "Jane Armless", new HumanName("Sr.", "Simão Armless",null)));
+        patient.setAddress(new Address("Rua 2", "123", "apt 201", "Bairro 1", "Cidade 1", "Estado 1", "12345678"));
+        patient.setContacts(new Contacts("qwe@email.com", new ArrayList<>(Arrays.asList(
+                new Phone("32", "12345678901", true),
+                new Phone("21", "12345678901", false)))));
+        patient.setBirthDate("02/02/2000");
+        patient.setActive(true);
+        patient.setCreatedAt(LocalDateTime.now());
+        patient.setCpf("12345678902");
+
+        return patient;
+    }
+
+    public PatientDTO createPatientDTO1() throws ParseException {
+        PatientDTO patientDTO = new PatientDTO();
+
+        patientDTO.setId("1");
+        patientDTO.setName(new HumanNameDTO(null, "John Doe", null));
+        patientDTO.setAddress(new AddressDTO("Rua 1", "123", "apt 201", "Bairro 1", "Cidade 1", "Estado 1", "12345678"));
+        patientDTO.setContacts(new ContactsDTO("qwe@email.com", new ArrayList<>(Arrays.asList(
+                new PhoneDTO("21", "12345678901", true),
+                new PhoneDTO("11", "12345678901", false)))));
+        patientDTO.setBirthDate("01/01/2000");
+        patientDTO.setCpf("12345678901");
+
+        return patientDTO;
+    }
+
+    public PatientDTO createPatientDTO2() throws ParseException {
+        PatientDTO patientDTO = new PatientDTO();
+
+        patientDTO.setId("2");
+        patientDTO.setName(new HumanNameDTO(null, "Jane Armless", new HumanNameDTO("Sr.", "Simão Armless",null)));
+        patientDTO.setAddress(new AddressDTO("Rua 2", "123", "apt 201", "Bairro 1", "Cidade 1", "Estado 1", "12345678"));
+        patientDTO.setContacts(new ContactsDTO("qwe@email.com", new ArrayList<>(Arrays.asList(
+                new PhoneDTO("32", "12345678901", true),
+                new PhoneDTO("21", "12345678901", false)))));
+        patientDTO.setBirthDate("02/02/2000");
+        patientDTO.setCpf("12345678902");
+
+        return patientDTO;
+    }
+}
